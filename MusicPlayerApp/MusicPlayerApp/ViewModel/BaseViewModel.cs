9884ca30208385f7a582735349a85ed04c7d1d17
Zubair Bester <zubairbester@hotmail.com>
2021-02-19 15:25:51 +0200

    adding all MusicPlayerApp Files to github

---
+++ MusicPlayerApp/MusicPlayerApp/ViewModel/BaseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using MusicPlayerApp.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace MusicPlayerApp.ViewModel
+{
+    public class BaseViewModel
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
